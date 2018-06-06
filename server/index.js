c00c923f397756443d8b9f7a810e1b4b5e6abb3b
Nick Gaudreau <yannickgaudreau@gmail.com>
2018-06-06 16:42:50 -0400

    Added Routes + basic impl./validation for login/signup

--- server/index.js
+++ server/index.js
@@ -1,8 +1,22 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 // tell the app to look for static files in these directories
 app.use(express.static('./static/'));
+
+// tell the app to parse HTTP body messages
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// routes
+const authRoutes = require('./routes/auth');
+app.use('/auth', authRoutes);
+
+// GET test
+app.get('/test',
+    (req, res) => {
+        res.send('Hi from express !');
+    });
 
 // start the server
 app.listen(3000, () => {
