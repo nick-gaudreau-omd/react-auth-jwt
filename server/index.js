c539140a27e04164cb0dcf3cb6ace0474388b469
Nick Gaudreau <yannickgaudreau@gmail.com>
2018-06-07 15:21:36 -0400

    Added CORS

--- server/index.js
+++ server/index.js
@@ -5,6 +5,15 @@ const app = express();
 // tell the app to look for static files in these directories
 app.use(express.static('./static/'));
 
+// cors - must be before app use routes
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+}
+app.use(allowCrossDomain);
+
 // tell the app to parse HTTP body messages
 app.use(bodyParser.urlencoded({ extended: false }));
 
